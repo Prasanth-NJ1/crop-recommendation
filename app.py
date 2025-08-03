42cdd56efe20105fd3ca162cd9d682100c4f6970
Prasanth-NJ1 <prasanthnj72@gmail.com>
2025-08-03 09:02:44 +0530

    Added CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 import os
@@ -7,6 +8,7 @@ with open('crop_recommendation_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
